70337cefb9ca92df99e846a8ce11ad4abcfe7e50
Siddharth <ahoy@barelyhuman.dev>
2021-11-11 13:09:52 +0530

    add dimension unit to hoc

--- src/hoc/with-spacery.js
+++ src/hoc/with-spacery.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { modsToStyle } from '../lib/mods-to-style'
 
-export function withSpacery (Component) {
+export function withSpacery (Component, dimensionUnit) {
   return ({ style, ...props }) => {
-    const { style: modifiersStyle, sanitizedProps } = modsToStyle(props)
+    const { style: modifiersStyle, sanitizedProps } = modsToStyle(
+      props,
+      dimensionUnit
+    )
     return (
       <Component style={{ ...modifiersStyle, ...style }} {...sanitizedProps} />
     )
