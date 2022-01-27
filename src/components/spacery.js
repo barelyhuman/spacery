29b3caee979eb02c371dbd96a5532bab5e162641
Reaper <ahoy@barelyhuman.dev>
2022-01-27 14:22:35 +0530

    fix: base component style overrides

--- src/components/spacery.js
+++ src/components/spacery.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { modsToStyle } from '../lib/mods-to-style.js'
 
-export function Spacery ({ children, ...props }) {
+export function Spacery ({ children, style, ...props }) {
   const { style: modifiersStyle, sanitizedProps } = modsToStyle(props)
   return (
-    <div style={modifiersStyle} {...sanitizedProps}>
+    <div style={{ ...modifiersStyle, ...style }} {...sanitizedProps}>
       {children}
     </div>
   )
